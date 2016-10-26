6fbbb0222ef75be687ee231d829a211d1ce3484e
hkr.fang <hkr.fang@gmail.com>
2016-10-25 21:25:34 -0700

    add some auth

--- src/main/scala/com/github/kfang/akkadir/routing/auth/AuthRoutes.scala
+++ src/main/scala/com/github/kfang/akkadir/routing/auth/AuthRoutes.scala
@@ -3,9 +3,20 @@ package com.github.kfang.akkadir.routing.auth
 import akka.http.scaladsl.server.Route
 import com.github.kfang.akkadir.AppPackage
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
+import akka.http.scaladsl.server.directives.Credentials
+import com.github.kfang.akkadir.models.user.User
 import com.github.kfang.akkadir.utils.AppRoutes
 
+import scala.concurrent.Future
+
 class AuthRoutes(implicit App: AppPackage) extends AppRoutes(App){
+
+  private def dbAuth(cred: Credentials): Future[Option[User]] = cred match {
+    case Credentials.Missing => Future.successful(None)
+    case Credentials.Provided(identifier) =>
+      //TODO: do something with the identifier
+      Future.successful(None)
+  }
 
   private val register: Route = (
     post &
@@ -15,8 +26,9 @@ class AuthRoutes(implicit App: AppPackage) extends AppRoutes(App){
 
   private val login: Route = (
     post &
-    path("login")
-  ){complete("")}
+    path("login") &
+    authenticateBasicAsync("", dbAuth)
+  ){(user) => complete("")}
 
 
 
